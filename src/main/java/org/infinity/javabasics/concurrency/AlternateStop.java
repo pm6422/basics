ba45c252ac82233586bd71bc09600ab649e874db
louis <louisliu@deepbrain.ai>
2018-10-25 21:51:29 +0800

    optimize thread pool

---
+++ src/main/java/org/infinity/javabasics/concurrency/AlternateStop.java
@@ -0,0 +1,41 @@
+package org.infinity.javabasics.concurrency;
+
+public class AlternateStop implements Runnable {
+    private volatile boolean stopRequested;
+    private          Thread  runThread;
+
+    public void run() {
+        runThread = Thread.currentThread();
+        stopRequested = false;
+        int count = 0;
+        while (!stopRequested) {
+            System.out.println("Running ... count=" + count);
+            count++;
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException x) {
+                Thread.currentThread().interrupt();// re-assert interrupt
+            }
+        }
+        System.out.println("Stopped");
+    }
+
+    public void stopRequest() {
+        stopRequested = true;
+        if (runThread != null) {
+//            runThread.interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        AlternateStop as = new AlternateStop();
+        Thread t = new Thread(as);
+        t.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException x) {
+            // ignore
+        }
+        as.stopRequest();
+    }
+}
