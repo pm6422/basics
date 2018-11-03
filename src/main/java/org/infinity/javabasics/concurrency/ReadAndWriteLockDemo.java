3d9c6c41a8a2583981c77e6d6ea2100d9ed750e0
louis <louisliu@deepbrain.ai>
2018-11-03 14:56:53 +0800

    new sample

---
+++ src/main/java/org/infinity/javabasics/concurrency/ReadAndWriteLockDemo.java
@@ -0,0 +1,71 @@
+package org.infinity.javabasics.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadAndWriteLockDemo {
+
+    public static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public static void main(String[] args) {
+        // 多个线程同时写
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                writeFile(Thread.currentThread());
+            }
+        });
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                writeFile(Thread.currentThread());
+            }
+        });
+    }
+
+    public static void readFile(Thread thread) {
+        lock.readLock().lock();
+        boolean readLock = lock.isWriteLocked();
+        if (!readLock) {
+            System.out.println("当前为读锁！");
+        }
+        try {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(thread.getName() + ":正在进行读操作……");
+            }
+            System.out.println(thread.getName() + ":读操作完毕！");
+        } finally {
+            System.out.println("释放读锁！");
+            lock.readLock().unlock();
+        }
+    }
+
+    public static void writeFile(Thread thread) {
+        lock.writeLock().lock();
+        boolean writeLock = lock.isWriteLocked();
+        if (writeLock) {
+            System.out.println("当前为写锁！");
+        }
+        try {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(thread.getName() + ":正在进行写操作……");
+            }
+            System.out.println(thread.getName() + ":写操作完毕！");
+        } finally {
+            System.out.println("释放写锁！");
+            lock.writeLock().unlock();
+        }
+    }
+}
