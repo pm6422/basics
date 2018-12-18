4cebf7562b2a25d0c8ecff81060e7ddc3a870302
louis <louisliu@deepbrain.ai>
2018-12-18 14:03:01 +0800

    optimize threads

--- src/main/java/org/infinity/javabasics/concurrency/ThreadAlternateRunDemo1.java
+++ src/main/java/org/infinity/javabasics/concurrency/ThreadAlternateRunDemo1.java
@@ -4,52 +4,47 @@ package org.infinity.javabasics.concurrency;
  * 注意使用Lock无法达到交替运行效果，只会达到一个线程执行完再执行下一个
  */
 public class ThreadAlternateRunDemo1 {
+    class EvenOddThread extends Thread {
 
-    private Object lock = new Object();
+        private Object lock;
 
-    public static void main(String[] args) {
-        new ThreadAlternateRunDemo1().run();
-    }
+        private int startIndex;
 
-    private void run() {
-        Thread threadEven = new Thread(() -> {
-            for (int i = 0; i <= 100; i = i + 2) {
+        public EvenOddThread(String name, Object lock, int startIndex) {
+            super.setName(name);
+            this.lock = lock;
+            this.startIndex = startIndex;
+        }
+
+        @Override
+        public void run() {
+            for (int i = startIndex; i <= 100; i = i + 2) {
                 synchronized (lock) {// 通过synchronized抢先获得到锁的线程进入RUNNABLE状态，未获得锁的处于BLOCKED状态
                     System.out.println(Thread.currentThread().getName() + "->" + i);
-                    System.out.println(Thread.currentThread().getName() + " notify");
+//                    System.out.println(Thread.currentThread().getName() + " notify");
                     lock.notify();// 调用notify后另外一个线程会处于BLOCKED状态，DEBUG模式后使用JVISUALVM可以看到，当本线程未释放对象锁的时候另外一个线程即使处于BLOCKED状态也不会运行
                     try {
-                        System.out.println(Thread.currentThread().getName() + " wait");
-                        lock.wait();// 调用该方法的线程进入WAITING状态并释放对象锁，然后线程暂停于此直到唤醒，另外处于WAITING状态的线程无法直接进入RUNNABLE状态，必须先进入BLOCKED状态，然后获取到monitor后才可以进入RUNNABLE状态
-                        System.out.println(Thread.currentThread().getName() + " obtained monitor");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, "threadEven");
-
-        Thread threadOdd = new Thread(() -> {
-            for (int i = 1; i <= 101; i = i + 2) {
-                synchronized (lock) {
-                    System.out.println(Thread.currentThread().getName() + "->" + i);
-                    System.out.println(Thread.currentThread().getName() + " notify");
-                    lock.notify();
-                    try {
-                        if (i < 101) {
-                            System.out.println(Thread.currentThread().getName() + " wait");
-                            lock.wait();
-                            System.out.println(Thread.currentThread().getName() + " obtained monitor");
+                        if (i < 100) {
+//                            System.out.println(Thread.currentThread().getName() + " wait");
+                            lock.wait();// 调用该方法的线程进入WAITING状态并释放对象锁，然后线程暂停于此直到唤醒，另外处于WAITING状态的线程无法直接进入RUNNABLE状态，必须先进入BLOCKED状态，然后获取到monitor后才可以进入RUNNABLE状态
+//                            System.out.println(Thread.currentThread().getName() + " obtained monitor");
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        }, "ThreadOdd");
+        }
+    }
+
+    private void run() {
+        Object lock = new Object();
+        new EvenOddThread("EvenThread", lock, 0).start();
+        new EvenOddThread("OddThread", lock, 1).start();
+    }
 
 
-        threadEven.start();
-        threadOdd.start();
+    public static void main(String[] args) {
+        new ThreadAlternateRunDemo1().run();
     }
 }
