3056a35f6cf2fadaa836ba91e7ff10972561a2c6
louis <louisliu@deepbrain.ai>
2018-12-24 19:35:42 +0800

    optimize threads

--- src/main/java/org/infinity/javabasics/concurrency/ThreadAlternateRunDemo3.java
+++ src/main/java/org/infinity/javabasics/concurrency/ThreadAlternateRunDemo3.java
@@ -8,14 +8,13 @@ public class ThreadAlternateRunDemo3 {
     private boolean evenFlag = true;
     private Lock    lock     = new ReentrantLock();
 
-    class EvenThread extends Thread {
+    public static void main(String[] args) {
+        new ThreadAlternateRunDemo3().run();
+    }
 
-        public EvenThread(String name) {
-            super.setName(name);
-        }
 
-        @Override
-        public void run() {
+    private void run() {
+        Thread t1 = new Thread(() -> {
             for (int i = 0; i <= 100; ) { // 第三个statement为空时，evenFlag又为false时形成了死循环，直到另外线程获得到锁后修改evenFlag为true。
                 if (evenFlag) {
                     try {
@@ -28,17 +27,9 @@ public class ThreadAlternateRunDemo3 {
                     }
                 }
             }
-        }
-    }
+        }, "EvenThread");
 
-    class OddThread extends Thread {
-
-        public OddThread(String name) {
-            super.setName(name);
-        }
-
-        @Override
-        public void run() {
+        Thread t2 = new Thread(() -> {
             for (int i = 1; i <= 101; ) {
                 if (!evenFlag) {
                     try {
@@ -51,17 +42,7 @@ public class ThreadAlternateRunDemo3 {
                     }
                 }
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        new ThreadAlternateRunDemo3().run();
-    }
-
-
-    private void run() {
-        Thread t1 = new EvenThread("EvenThread");
-        Thread t2 = new OddThread("OddThread");
+        }, "OddThread");
 
         t1.start();
         t2.start();
