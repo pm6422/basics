c8fb0ebc373c6fce1c5464982fc3c17d1759ac7f
louis <louisliu@deepbrain.ai>
2019-01-26 11:03:37 +0800

    optimize threads

---
+++ src/main/java/org/infinity/javabasics/concurrency/ConditionDemo.java
@@ -0,0 +1,54 @@
+package org.infinity.javabasics.concurrency;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionDemo {
+    private Lock      lock      = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public static void main(String[] args) throws Exception {
+        ConditionDemo demo = new ConditionDemo();
+        MyThread thread = new MyThread(demo);
+        thread.start();
+        TimeUnit.SECONDS.sleep(3);
+        demo.signal(); // condition.signal()等效于object.notify()
+    }
+
+    public void await() {
+        try {
+            lock.lock();
+            System.out.println("await时间为：" + System.currentTimeMillis());
+            condition.await(); // condition.await()等效于object.wait()，Condition的await()方法会释放锁。调用后暂停执行，直到condition.signal()
+            System.out.println("await等待结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void signal() {
+        try {
+            lock.lock();
+            System.out.println("signal时间为：" + System.currentTimeMillis());
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+class MyThread extends Thread {
+    private ConditionDemo demo;
+
+    public MyThread(ConditionDemo demo) {
+        this.demo = demo;
+    }
+
+    public void run() {
+        demo.await();
+    }
+}
