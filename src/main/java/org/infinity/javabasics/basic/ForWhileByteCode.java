9634151b7fc5638e7379c0a829c255a6750b9bc8
louis <louisliu@deepbrain.ai>
2019-01-28 11:09:46 +0800

    optimize threads

---
+++ src/main/java/org/infinity/javabasics/basic/ForWhileByteCode.java
@@ -0,0 +1,16 @@
+package org.infinity.javabasics.basic;
+
+public class ForWhileByteCode {
+
+    public void func1() {
+        for (; ; ) {
+            System.out.println("for loop");
+        }
+    }
+
+    public void func2() {
+        while (true) {
+            System.out.println("while loop");
+        }
+    }
+}
