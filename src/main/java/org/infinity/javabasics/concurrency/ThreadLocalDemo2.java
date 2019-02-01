801428f73d8f2e140e6f55f2df3468e60b326ecc
louis <louisliu@deepbrain.ai>
2019-02-01 20:27:49 +0800

    refactor

--- src/main/java/org/infinity/javabasics/concurrency/ThreadLocalDemo2.java
+++ src/main/java/org/infinity/javabasics/concurrency/ThreadLocalDemo2.java
@@ -1,4 +1,21 @@
 package org.infinity.javabasics.concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadLocalDemo2 {
+
+    private static List<String> list = new ArrayList<>();
+
+    private static final ThreadLocal<List<String>> threadLocal = ThreadLocal.withInitial(() -> list);
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get().size());// 结果为0
+        list.add("sssd");
+        System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get().size());// 结果为1
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get().size());// 结果为1
+        }).start();
+    }
 }
