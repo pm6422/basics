ff072390c8897439693f731b40aec7e5b79d6b3f
louis <louisliu@deepbrain.ai>
2019-02-05 21:25:09 +0800

    refactor

---
+++ src/main/java/org/infinity/javabasics/concurrency/ThreadPoolExecutorDemo2.java
@@ -0,0 +1,23 @@
+package org.infinity.javabasics.concurrency;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolExecutorDemo2 {
+
+    public static void main(String[] args) {
+        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(10);
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ExecutorService threadPool = new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS,
+                workingQueue, rejectedExecutionHandler);
+
+        for (int i = 0; i < 100; i++) {
+            final int threadNo = i;
+            threadPool.submit((Callable<Boolean>) () -> {
+                System.out.println("thread " + String.valueOf(threadNo) + " is called");
+                Thread.sleep(10000);
+                System.out.println("thread " + String.valueOf(threadNo) + " is awake");
+                throw new Exception();
+            });
+        }
+    }
+}
