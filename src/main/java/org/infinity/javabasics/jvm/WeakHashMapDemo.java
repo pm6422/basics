47db98f1505e2f7f1dcc192986bc4fa5e868ddd1
louis <louisliu@deepbrain.ai>
2019-02-17 22:06:02 +0800

    refactor

--- src/main/java/org/infinity/javabasics/jvm/WeakHashMapDemo.java
+++ src/main/java/org/infinity/javabasics/jvm/WeakHashMapDemo.java
@@ -7,18 +7,21 @@ public class WeakHashMapDemo {
 
     public static void main(String[] args) throws InterruptedException {
         WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
-        String key1 = new String("key1");
+        String key1 = new String("key1");//new String("key1")会在堆区创建一个可以被正常回收的对象
         String key2 = new String("key2");
         String key3 = new String("key3");
+        String key4 = "key4";//String直接赋值，会在Java StringPool(字符串常量池)里创建一个String对象，通常不会被gc回收。思考一下原因？
         weakHashMap.put(key1, "value1");
         weakHashMap.put(key2, "value2");
         weakHashMap.put(key3, "value3");
+        weakHashMap.put(key4, "value4");
         System.out.println("before gc weakHashMap = " + weakHashMap + " , size=" + weakHashMap.size());
         // 使没有任何强引用指向key1
         key1 = null;
+        key4 = null;
         // 通知JVM的gc进行垃圾回收
         System.gc();
         TimeUnit.SECONDS.sleep(1);
-        System.out.println("after gc weakHashMap = " + weakHashMap + " , size=" + weakHashMap.size()); //输出：size=2
+        System.out.println("after gc weakHashMap = " + weakHashMap + " , size=" + weakHashMap.size()); //输出：size=3
     }
 }
--- src/main/java/org/infinity/javabasics/jvm/WeakReferenceDemo.java
+++ src/main/java/org/infinity/javabasics/jvm/WeakReferenceDemo.java
@@ -1,13 +1,28 @@
 package org.infinity.javabasics.jvm;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 
 public class WeakReferenceDemo {
     public static void main(String[] args) {
         Object reference = new Object();
         System.out.println(reference);
-        WeakReference<Object> weakReference = new WeakReference<>(reference);
+        ReferenceQueue referenceQueue = new ReferenceQueue();
+        WeakReference<Object> weakReference = new WeakReference<>(reference, referenceQueue);
         reference = null;
+
+        // 用于检查引用队列中的引用值被回收
+        Thread checkRefQueueThread = new Thread(() -> {
+            while (true) {
+                Reference<? extends String> clearRef = referenceQueue.poll();
+                if (null != clearRef) {
+                    System.out.println("引用对象被回收, ref = " + clearRef + ", value = " + clearRef.get());
+                }
+            }
+        });
+        checkRefQueueThread.start();
+
         System.gc();
         System.out.println(weakReference.get());// null
     }
