08d540bcc11e843091be2a101680b11d20889407
louis <louisliu@deepbrain.ai>
2019-02-17 21:54:31 +0800

    refactor

--- src/main/java/org/infinity/javabasics/jvm/WeakHashMapDemo.java
+++ src/main/java/org/infinity/javabasics/jvm/WeakHashMapDemo.java
@@ -6,15 +6,19 @@ import java.util.concurrent.TimeUnit;
 public class WeakHashMapDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        Employee employee = new Employee("aaa");
-        WeakHashMap map = new WeakHashMap();
-        map.put(employee, "ss");
-        System.out.println(map.size());
+        WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+        String key1 = new String("key1");
+        String key2 = new String("key2");
+        String key3 = new String("key3");
+        weakHashMap.put(key1, "value1");
+        weakHashMap.put(key2, "value2");
+        weakHashMap.put(key3, "value3");
+        System.out.println("before gc weakHashMap = " + weakHashMap + " , size=" + weakHashMap.size());
+        // 使没有任何强引用指向key1
+        key1 = null;
+        // 通知JVM的gc进行垃圾回收
         System.gc();
-        System.out.println(map.size());
-        employee = null;
-        System.gc();
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println(map.size()); // 输出0
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("after gc weakHashMap = " + weakHashMap + " , size=" + weakHashMap.size()); //输出：size=2
     }
 }
