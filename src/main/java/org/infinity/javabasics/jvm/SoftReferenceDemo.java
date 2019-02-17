b19cc8aa3467278105b5ca018f387a1bf137e792
louis <louisliu@deepbrain.ai>
2019-02-17 21:47:28 +0800

    refactor

--- src/main/java/org/infinity/javabasics/jvm/SoftReferenceDemo.java
+++ src/main/java/org/infinity/javabasics/jvm/SoftReferenceDemo.java
@@ -1,6 +1,8 @@
 package org.infinity.javabasics.jvm;
 
 import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoftReferenceDemo {
 
@@ -11,5 +13,30 @@ public class SoftReferenceDemo {
         reference = null;
         System.gc();
         System.out.println(softReference.get());//强制GC之后，SoftReference所引用的对象并没有被回收
+    }
+
+    /**
+     * 正确使用引用对象demo
+     */
+    private static void correctUseRefObjDemo() {
+        List<String> myList = new ArrayList<>();
+        SoftReference<List<String>> refObj = new SoftReference<>(myList);
+        List<String> list = refObj.get();// 正确的使用，使用强引用指向对象保证获得对象之后不会被回收
+        if (null != list) {
+            list.add("hello");
+        } else {
+            // 整个列表已经被垃圾回收了，做其他处理
+        }
+    }
+
+    /**
+     * 错误使用引用对象demo
+     */
+    private static void incorrectUseRefObjDemo() {
+        List<String> myList = new ArrayList<>();
+        SoftReference<List<String>> refObj = new SoftReference<>(myList);
+        if (null != refObj.get()) {// 错误的使用，在检查对象非空到使用对象期间，对象可能已经被回收.可能出现空指针异常
+            refObj.get().add("hello");
+        }
     }
 }
