159e5ec452b286acb19f15dc48587c1824b4fe67
louis <louisliu@deepbrain.ai>
2019-02-27 19:38:55 +0800

    refactor

--- src/main/java/org/infinity/javabasics/collection/ArrayListConcurrentModificationExceptionDemo.java
+++ src/main/java/org/infinity/javabasics/collection/ArrayListConcurrentModificationExceptionDemo.java
@@ -1,20 +1,32 @@
 package org.infinity.javabasics.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.google.common.collect.Lists;
+
 import java.util.List;
 
 public class ArrayListConcurrentModificationExceptionDemo {
 
     public static void main(String[] args) {
-        List<String> all = new ArrayList<>();
-        List<String> list = Arrays.asList("QQ", "AA", "WW", "QQ", "AA", "WW");
+//      List<String> list = Arrays.asList("QQ", "AA", "WW", "QQ", "AA", "WW");// 这个版本调用removeIf会报UnsupportedOperationException
+        List<String> list = Lists.newArrayList("QQ", "AA", "WW", "QQ", "AA", "WW");
 
-        all.addAll(list);
-        for (String str : all) {
-            if (str.equals("QQ")) {
-                all.remove(str);
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                list.add("" + i);
             }
-        }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                list.removeIf(x -> x.equals("QQ"));
+            }
+
+            // the below statement is ok
+//            list.stream().filter(x-> x.equals("QQ")).collect(Collectors.toList());
+        });
+
+        t1.start();
+        t2.start();
+
     }
 }
