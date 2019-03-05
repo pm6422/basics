1dcc4fe75bc70a4848cc94dcdcacc4b9285c055f
louis <louisliu@deepbrain.ai>
2019-03-05 09:43:53 +0800

    refactor

--- src/main/java/org/infinity/javabasics/concurrency/ThreadPoolExecutorDemo.java
+++ src/main/java/org/infinity/javabasics/concurrency/ThreadPoolExecutorDemo.java
@@ -1,43 +1,59 @@
 package org.infinity.javabasics.concurrency;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorDemo {
     public static void main(String[] args) throws InterruptedException {
-        Runnable myRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                    System.out.println(Thread.currentThread().getName() + " run");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Runnable myRunnable = () -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         };
 
 //        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new SynchronousQueue<>());
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
-        executor.execute(myRunnable);
-        executor.execute(myRunnable);
-        executor.execute(myRunnable);
-        System.out.println("---先开3个---");
-        System.out.println("核心线程数" + executor.getCorePoolSize());
-        System.out.println("线程池数" + executor.getPoolSize());
-        System.out.println("队列任务数" + executor.getQueue().size());
-        executor.execute(myRunnable);
-        executor.execute(myRunnable);
-        executor.execute(myRunnable);
-        System.out.println("---再开3个---");
-        System.out.println("核心线程数" + executor.getCorePoolSize());
-        System.out.println("线程池数" + executor.getPoolSize());
-        System.out.println("队列任务数" + executor.getQueue().size());
-        Thread.sleep(8000);
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2), new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        int size = 3;
+        for (int i = 0; i < size; i++) {
+            threadPool.execute(myRunnable);
+        }
+        System.out.println("---启动" + size + "个任务---");
+        System.out.println("线程池数" + threadPool.getPoolSize());
+        System.out.println("队列任务数" + threadPool.getQueue().size());
+
+        size = 3;
+        for (int i = 0; i < size; i++) {
+            threadPool.execute(myRunnable);
+        }
+        System.out.println("---启动" + size + "个任务---");
+        System.out.println("线程池数" + threadPool.getPoolSize());
+        System.out.println("队列任务数" + threadPool.getQueue().size());
+
+        size = 2;
+        for (int i = 0; i < size; i++) {
+            threadPool.execute(myRunnable);
+        }
+        System.out.println("---启动" + size + "个任务---");
+        System.out.println("线程池数" + threadPool.getPoolSize());
+        System.out.println("队列任务数" + threadPool.getQueue().size());
+
+        size = 5;
+        for (int i = 0; i < size; i++) {
+            threadPool.execute(myRunnable);
+        }
+        System.out.println("---启动" + size + "个任务---");
+        System.out.println("线程池数" + threadPool.getPoolSize());
+        System.out.println("队列任务数" + threadPool.getQueue().size());
+
+        TimeUnit.SECONDS.sleep(8);
         System.out.println("----8秒之后----");
-        System.out.println("核心线程数" + executor.getCorePoolSize());
-        System.out.println("线程池数" + executor.getPoolSize());
-        System.out.println("队列任务数" + executor.getQueue().size());
+        System.out.println("线程池数" + threadPool.getPoolSize());
+        System.out.println("队列任务数" + threadPool.getQueue().size());
+
+        threadPool.shutdown();
     }
 }
