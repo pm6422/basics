c8dd94d5a91fde4b037f1b0c9d7afeeade695a1c
louis <louisliu@deepbrain.ai>
2019-10-10 13:51:56 +0800

    new file

---
+++ src/main/java/org/infinity/javabasics/io/CloneDemo.java
@@ -0,0 +1,33 @@
+package org.infinity.javabasics.io;
+
+public class CloneDemo {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Dog dog = new Dog();
+        dog.name = "旺财";
+        dog.age = 5;
+        // 克隆
+        Dog dog3 = (Dog) dog.clone();
+        dog3.name = "小白";
+        dog3.age = 2;
+        System.out.println(dog);
+        System.out.println(dog3);
+    }
+}
+
+class Dog implements Cloneable {
+    public String name;
+    public int    age;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
