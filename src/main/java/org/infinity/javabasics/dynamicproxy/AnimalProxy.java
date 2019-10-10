7ed2731590352627c689efc979618f2ba8ff0e63
louis <louisliu@deepbrain.ai>
2019-10-10 17:19:01 +0800

    new file

---
+++ src/main/java/org/infinity/javabasics/dynamicproxy/AnimalProxy.java
@@ -0,0 +1,51 @@
+package org.infinity.javabasics.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+// JDK代理类
+public class AnimalProxy implements InvocationHandler {
+
+    private Object target; // 代理对象
+
+    public Object getInstance(Object target) {
+        this.target = target;
+        // 取得代理对象
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("调用前");
+        Object result = method.invoke(target, args); // 方法调用
+        System.out.println("调用后");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // JDK 动态代理调用
+        AnimalProxy proxy = new AnimalProxy();
+        Animal dogProxy = (Animal) proxy.getInstance(new Dog());
+        dogProxy.eat();
+    }
+}
+
+interface Animal {
+    void eat();
+}
+
+class Dog implements Animal {
+    @Override
+    public void eat() {
+        System.out.println("The dog is eating");
+    }
+}
+
+class Cat implements Animal {
+    @Override
+    public void eat() {
+        System.out.println("The cat is eating");
+    }
+}
+ 
