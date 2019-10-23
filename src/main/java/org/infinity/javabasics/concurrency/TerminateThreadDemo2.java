301652c55bc6c177cfd1ce23d98d02c4accfbdc6
louis <louisliu@deepbrain.ai>
2019-10-23 13:45:26 +0800

    refactor

---
+++ src/main/java/org/infinity/javabasics/concurrency/TerminateThreadDemo2.java
@@ -0,0 +1,35 @@
+package org.infinity.javabasics.concurrency;
+
+public class TerminateThreadDemo2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    // 使用中断机制，来终止线程
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("Interrupted ...");
+                        break;
+                    }
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        System.out.println("Interrupted when sleep ...");
+                        // Thread.sleep()方法由于中断抛出异常。
+                        // JVM会先将该线程的中断标识位清除，然后抛出InterruptedException，
+                        // 因为在发生InterruptedException异常的时候，会清除中断标记
+                        // 如果不加处理，那么下一次循环开始的时候，就无法捕获这个异常。
+                        // 故在异常处理中，再次设置中断标记位
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        };
+
+        // 开启线程
+        thread.start();
+        Thread.sleep(2000);
+        thread.interrupt();
+    }
+}
