268b1a00623a73483159b8035e2e57c711e1d3af
louis <louisliu@deepbrain.ai>
2019-11-27 10:44:32 +0800

    refactor

--- src/main/java/org/infinity/javabasics/io/ShadowCloneDemo.java
+++ src/main/java/org/infinity/javabasics/io/ShadowCloneDemo.java
@@ -2,20 +2,23 @@ package org.infinity.javabasics.io;
 
 public class ShadowCloneDemo {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Dog dog = new Dog();
-        dog.name = "旺财";
-        dog.age = 5;
-        // 克隆
-        Dog dog3 = (Dog) dog.clone();
-        dog3.name = "小白";
-        dog3.age = 2;
-        System.out.println(dog);
-        System.out.println(dog3);
+        Dog dog1 = new Dog();
+        dog1.name = "旺财";
+        dog1.age = 5;
+
+        Dog dog2 = (Dog) dog1.clone(); // 调用clone()方法实现克隆
+        dog2.name = "小白";
+        dog2.age = 2;
+        System.out.println(dog1);
+        System.out.println(dog2);
     }
 }
 
+/**
+ * 注意：必须实现Cloneable接口，否则无法调用clone()方法
+ */
 class Dog implements Cloneable {
-    public String name;
+    public String name;// 注意String类型的字段值也会克隆复制
     public int    age;
 
     @Override
