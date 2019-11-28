3833a6b18c4b3966240d85b6f820a70c88620510
louis <louisliu@deepbrain.ai>
2019-11-28 13:47:23 +0800

    refactor

--- src/main/java/org/infinity/javabasics/basic/StringAssignment.java
+++ src/main/java/org/infinity/javabasics/basic/StringAssignment.java
@@ -5,7 +5,7 @@ public class StringAssignment {
     public static void main(String[] args) {
         String s = "123";
         changeString(s);
-        System.out.println(s);
+        System.out.println(s); // 输出结果: 123
     }
 
     private static void changeString(String s) {
