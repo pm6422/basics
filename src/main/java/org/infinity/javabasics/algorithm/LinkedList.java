a0a092b4ed02e7e3c668ac5d2581645b3cd3f749
Louis <pm6422@126.com>
2020-02-14 19:26:14 +0800

    refactor

---
+++ src/main/java/org/infinity/javabasics/algorithm/LinkedList.java
@@ -0,0 +1,16 @@
+package org.infinity.javabasics.algorithm;
+
+public class LinkedList {
+    public void removeDuplicatedElements(Node head) {
+
+    }
+
+    class Node {
+        private String val;
+        private Node   next;
+
+        Node(String val) {
+            this.val = val;
+        }
+    }
+}
